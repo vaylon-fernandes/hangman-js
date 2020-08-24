b0a11309b15ece14448770082c38ed48c414a539
JuliusAlphonso <JadeMaveric@users.noreply.github.com>
2020-08-24 20:18:05 +0530

    Make flower react to mistakes

--- hangman.js
+++ hangman.js
@@ -3,26 +3,34 @@
 * Author: @jelofsson
 **/
 var Hangman = (function () {
-    
+
     'use strict';
-               
+
     function Hangman(elId) {
-        
+
         // Dom is ready
         this.elId       = elId;
         this.words      = [];
         this.hints      = [];
 
         fetch('/wordlist.json')
-        .then( res => res.json() )
-        .then( json => json.forEach( entry => {
-            this.words.push(entry.word);
-            this.hints.push(entry.hint);
-        }))
+            .then( res => res.json() )
+            .then( json => json.forEach( entry => {
+                this.words.push(entry.word);
+                this.hints.push(entry.hint);
+            }))
+        
+        // Hide the flower
+        this.hideElementById(this.elId + "_1", null)
+        this.hideElementById(this.elId + "_2", null)
+        this.hideElementById(this.elId + "_3", null)
+        this.hideElementById(this.elId + "_4", null)
+        this.hideElementById(this.elId + "_5", null)
+        this.hideElementById(this.elId + "_6", null)
     }
 
     Hangman.prototype.reset = function () {
-        
+
         // Reset variables
         this.STOPPED        = false;
         this.MISTAKES       = 0;
@@ -35,13 +43,21 @@ var Hangman = (function () {
         this.hideElementByClass('h');
         this.showElementByIdWithContent(this.elId + "_guessbox", null);
         this.showElementByIdWithContent(this.elId + "_word", this.getGuessedfWord());
-        document.getElementById(this.elId + "_hintbox").innerHTML = "<button onclick='Hangman.showHint(); return false'>HINT</button>"
+        this.showElementByIdWithContent(this.elId + "_hintbox", "<button onclick='Hangman.showHint(); return false'>HINT</button>");
+
+        // Show the flower
+        this.showElementByIdWithContent(this.elId + "_1", null)
+        this.showElementByIdWithContent(this.elId + "_2", null)
+        this.showElementByIdWithContent(this.elId + "_3", null)
+        this.showElementByIdWithContent(this.elId + "_4", null)
+        this.showElementByIdWithContent(this.elId + "_5", null)
+        this.showElementByIdWithContent(this.elId + "_6", null)
     };
 
     Hangman.prototype.showHint = function () {
 
         // Replace button with hint text
-        document.getElementById(this.elId + "_hintbox").innerHTML = this.HINT;
+        this.showElementByIdWithContent(this.elId + "_hintbox", this.HINT);
     }
 
     Hangman.prototype.guess = function (guess) {
@@ -62,12 +78,12 @@ var Hangman = (function () {
         this.showElementByIdWithContent(this.elId + "_guesses", this.GUESSES.join(''));
 
         if (this.WORD.indexOf(guess) < 0) {
-            
+
             // Incorrect guess
             this.MISTAKES++;
-            
-            // Show next part of hangman character
-            this.showElementByIdWithContent(this.elId + "_" + this.MISTAKES, null);
+
+            // Hide a petal
+            this.hideElementById(this.elId + "_" + this.MISTAKES, null);
 
             if (this.MISTAKES === 6) {
                 // Game Over
@@ -75,7 +91,7 @@ var Hangman = (function () {
                 this.STOPPED = true;
                 return;
             }
-            
+
         } else if (this.WORD.indexOf(this.getGuessedfWord()) !== -1) {
             // Victory
             this.showElementByIdWithContent(this.elId + "_end", "You made it!<br/>The word was: " + this.WORD);
@@ -84,14 +100,18 @@ var Hangman = (function () {
         }
 
     };
-    
+
     Hangman.prototype.showElementByIdWithContent = function (elId, content) {
         if (content !== null) {
             document.getElementById(elId).innerHTML = content;
         }
         document.getElementById(elId).style.opacity = 1;
     };
-    
+
+    Hangman.prototype.hideElementById = function (elId) {
+        document.getElementById(elId).style.opacity = 0;
+    }
+
     Hangman.prototype.hideElementByClass = function (elClass) {
         var elements = document.getElementsByClassName(elClass), i;
         for (i = 0; i < elements.length; i++) {
@@ -104,11 +124,11 @@ var Hangman = (function () {
         for (i = 0; i < this.WORD.length; i++) {
             // Word characters
             result += (this.GUESSES.indexOf(this.WORD[i]) > -1) ?
-                    this.WORD[i] : "_";
+                this.WORD[i] : "_";
         }
         return result;
     };
-    
+
     return new Hangman('hangm');
-    
+
 }());
